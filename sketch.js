5f5f32b56720db0c9131e4bd577766ca8b339b76
Cintronny <42981001+Cintronny@users.noreply.github.com>
2018-12-13 13:39:24 -0500

    Final

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+///The image I have decided to create is a snowglobe that encapsulates the rutgers "R" symbol. With it being the end of the semester
+//and the holiday season upon us, I thought that a snowglob would be fitting. As you slowly drag your cursor accross the screen. watch as the snow drifts around.
+//After an hour passes and you are no longer mesmorized by the incredible realism of the snow, kick back and enjoy yourself a hot chocolate.
+
+
+
+
+//create vars that can be used throughout the code
 var img;
-var snowlocation = 0
+
 var snowmove = 0
 
 function preload() {
-  img = loadImage('rutgerssymbol.png');
+img = loadImage('rutgerssymbol.png');
 }
 function setup() {
 
@@ -19,13 +27,13 @@ function draw() {
 
 	rotate(2.350);
 	var c = color(255,255, 160); // Define color 'c'
-fill(c); // Use color variable 'c' as fill color
+fill(c);
 	strokeWeight(2);
-stroke(14); // Don't draw a stroke around shapes
+stroke(14); //wanted there to be a boarder
 	arc(0, 0, 200, 200, 0, HALF_PI + PI, CHORD);
 	beginShape();
-c = color(255, 255, 255); // Update 'c' with grayscale value
-fill(c);
+c = color(255, 255, 255);
+fill(c); //Mapping of the base of the snowglobe
 vertex(1, -150);
 vertex(2, -98);
 vertex(100, 1);
@@ -34,6 +42,7 @@ endShape(CLOSE);
 
 
 rotate(3.95);{
+  //Had to rotate the image to get it to lay flat on the bas of the snowglob
 image(img, -25, 10, 60, 60);
 }
 
@@ -42,60 +51,61 @@ c = color(255, 255, 255); // Update 'c' with grayscale value
 fill(c);
 stroke(255, 255, 255);
 strokeWeight(4);
-	point(30, 20);
-point(80 + snowlocation, 20 + snowmove);
-point(85 + snowlocation, 50 + snowmove);
-point(30 + snowlocation, 75 + snowmove);
-point(80 + snowlocation, 40 + snowmove);
-point(50 + snowlocation, 50 + snowmove);
-point(20 + snowlocation, 20 + snowmove);
-point(-50 + snowlocation, 20 + snowmove);
-point(-60 + snowlocation, 35 + snowmove);
-point(-75 + snowlocation, 10 + snowmove);
-point(-80 + snowlocation, 20 + snowmove);
-point(80 + snowlocation, 1 + snowmove);
-point(85 + snowlocation, 10 + snowmove);
-point(30 + snowlocation, 90 + snowmove);
-point(36 + snowlocation, 40 + snowmove);
-point(36 + snowlocation, -10 + snowmove);
-point(-10 + snowlocation, 20 + snowmove);
-point(-50 + snowlocation, 20 + snowmove);
-point(-60 + snowlocation, 10 + snowmove);
-point(-75 + snowlocation, -40 + snowmove);
-point(-60 + snowlocation, -70 + snowmove);
-point(-75 + snowlocation, 10 + snowmove);
-point(-80 + snowlocation, 20 + snowmove);
-point(70 + snowlocation, -10 + snowmove);
-point(85 + snowlocation, 0 + snowmove);
-point(0 + snowlocation, 90 + snowmove);
-point(0 + snowlocation, -45 + snowmove);
-point(0 + snowlocation, -10 + snowmove);
-point(0 + snowlocation, -20 + snowmove);
-point(0 + snowlocation, -90 + snowmove);
-point(0 + snowlocation, -6 + snowmove);
-point(0 + snowlocation, -40 + snowmove);
-point(20 + snowlocation, 0 + snowmove);
-point(0 + snowlocation, 40 + snowmove);
-point(-75 + snowlocation, 0 + snowmove);
-point(-60 + snowlocation, 0 + snowmove);
-point(-75 + snowlocation,0 + snowmove);
-point(-80 + snowlocation, 0 + snowmove);
-point(70 + snowlocation, 0 + snowmove);
-point(85 + snowlocation, 0 + snowmove);
-point(0 + snowlocation, 90 + snowmove);
-point(0 + snowlocation, -45 + snowmove);
-point(0 + snowlocation, -10 + snowmove);
-point(0 + snowlocation, -20 + snowmove);
-point(0 + snowlocation, -90 + snowmove);
-point(0 + snowlocation, -6 + snowmove);
-point(0 + snowlocation, -40 + snowmove);
-point(20 + snowlocation, 0 + snowmove);
+
+//Here are a ton of points that are the snowflakes
+point(30, 20);
+point(80, 20 + snowmove);
+point(85, 50 + snowmove);
+point(30, 75 + snowmove);
+point(80, 40 + snowmove);
+point(50, 50 + snowmove);
+point(20, 20 + snowmove);
+point(-50, 20 + snowmove);
+point(-60, 35 + snowmove);
+point(-75, 10 + snowmove);
+point(-80, 20 + snowmove);
+point(80, 1 + snowmove);
+point(85, 10 + snowmove);
+point(30, 90 + snowmove);
+point(36, 40 + snowmove);
+point(36, -10 + snowmove);
+point(-10, 20 + snowmove);
+point(-50, 20 + snowmove);
+point(-6, 10 + snowmove);
+point(-7, -40 + snowmove);
+point(-6, -70 + snowmove);
+point(-7, 10 + snowmove);
+point(-8, 20 + snowmove);
+point(70 , -10 + snowmove);
+point(85, 0 + snowmove);
+point(0, 90 + snowmove);
+point(0, -45 + snowmove);
+point(0, -10 + snowmove);
+point(0, -20 + snowmove);
+point(0, -90 + snowmove);
+point(0, -6 + snowmove);
+point(0, -40 + snowmove);
+point(20, 0 + snowmove);
+point(0, 40 + snowmove);
+point(-75, 0 + snowmove);
+point(-60, 0 + snowmove);
+point(-75,0 + snowmove);
+point(-80, 0 + snowmove);
+point(70, 0 + snowmove);
+point(85, 0 + snowmove);
+point(0, 90 + snowmove);
+point(0, -45 + snowmove);
+point(0, -10 + snowmove);
+point(0, -20 + snowmove);
+point(0, -90 + snowmove);
+point(0, -6 + snowmove);
+point(0, -40 + snowmove);
+point(20 , 0 + snowmove);
 
 }
-function mouseMoved() {
-   snowlocation = snowlocation + 1
 
-}
-function mousePressed(){
-    snowmove = snowlocation + 9
+
+function mouseMoved(){
+    snowmove = random(-5, 50);
+//the random function makes it so that when the snow moves it does not only move in one direction.
 }
